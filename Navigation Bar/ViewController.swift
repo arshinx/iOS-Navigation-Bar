68837ad7aec1a0358b4ac828c8d728867fc0fbd3
Arshin Jain <arshin.jain@outlook.com>
2016-07-05 21:10:18 -0700

    create timer

--- Navigation Bar/ViewController.swift
+++ Navigation Bar/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         var timer = NSTimer()
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("result"), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
